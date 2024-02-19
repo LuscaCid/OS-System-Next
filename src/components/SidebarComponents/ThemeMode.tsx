272e269740373f3f953e65f412203a9670cac334
Lucas Felipe Lima Cid <lucasfelipaa@gmail.com>
2024-02-19 10:15:36 -0300

    adds toggle theme

---
+++ src/components/SidebarComponents/ThemeMode.tsx
@@ -0,0 +1,13 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
+export function ChangeTheme() {
+    return (
+        <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+                 
+            </Dialog.Content>
+        </Dialog.Portal>
+
+    )
+}
