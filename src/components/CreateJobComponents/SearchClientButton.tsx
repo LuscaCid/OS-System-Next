195e59a9f28ac469ea13427e36c3b13d0ceb44a2
Lucas Felipe Lima Cid <lucasfelipaaa@gmail.com>
2024-02-18 23:03:01 -0300

    search client button changed

--- src/components/CreateJobComponents/SearchClientButton.tsx
+++ src/components/CreateJobComponents/SearchClientButton.tsx
@@ -7,8 +7,8 @@ export function SearchClientButton () {
         <section className='flex flex-col gap-2 mt-4'>
             <Dialog.Root>
                 <Dialog.Trigger asChild>
-                    <button type='button' className=' items-center flex  w-full bg-transparent p-3 border border-zinc-300 rounded-md hover:bg-zinc-200 transition duration-200'>
-                        <Search strokeWidth={1.25} size={24}/> Select an Client 
+                    <button type='button' className=' items-center flex justify-between  w-full bg-transparent p-3 border border-zinc-300 rounded-md hover:bg-zinc-200/50 transition duration-200 cursor-text'>
+                        Select an Client <Search strokeWidth={1.25} size={24}/>
                     </button>
                 </Dialog.Trigger>    
             <SearchClientModal />
